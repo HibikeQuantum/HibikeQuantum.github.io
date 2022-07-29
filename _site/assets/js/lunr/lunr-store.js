52ce4b9fc186fa1935c42238e9b184174fc5f9c6
HibikeQuantum <jadetypehoon@gmail.com>
2022-07-30 00:26:14 +0900

    New build and auto commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -11,9 +11,9 @@ var store = [{
         "url": "/jekyll/update/welcome-to-jekyll/",
         "teaser": "/assets/images/HibikeQauntumSymbol_500x300_version_waifu.jpeg"
       },{
-        "title": "2022.07. 28 TIL & Diary",
+        "title": "2022.07.29 TIL & Diary",
         "excerpt":"2022.07. 28 TIL &amp; Diary  jekll 설정, 무전치사, 그래도 미국, 일본 부동산  Language  m1 환경에서 루비 설치하느라 한번 뒹굴고 jekyll theme config에 들어갈 검색엔진 등록 등 작업을 하는데 주로 시간을 보냈다. 정적 블로그를 써보는건 처음이라 기능들을 익히느라 정신이 없다. 바이두 가입도 했다. 클라우드 서비스는 한국 전화번호로도 가능하더라. 가격만 싸면 바이두에다 정적 파일들을 올려놓고 쓰는것도 나쁘지 않을듯. 보안이 걱정되면 암호화해서 올리는 방법도 있을테니.  Shell  rev, cut, paste  Philosophy  이 책을 읽으면 은근히 중국에 대해 관대하게 평가하는것 처럼 느껴지면서도 그렇다고 미국에 당장 넘어가는 시나리오까지는 가지 않는 모습을 수치적으론 예상한다. 그가 웹에 공개한 20년 뒤 제국수치를 볼때 여전히 미국이 앞서고 있는게 그렇다. 말은 중국의 리드를 말하면서도 수치로는 그렇지 않게 말하는 건데. 작가는 중국을 너무 무시하는 서구와 그 동맹국 독자에게 경고를 하면서도 객관적으론 공산당이 미국에 이기지 못할거란 생각을 하는 것일수도 있겠다.  English  무전치사 파트를 읽었다. 이 책이 참 좋다. 예전엔 이런 시선으로 영어를 생각해본적이 없었는데.. 그냥 경험이 쌓이고 쌓여 아마 그런건가 했던 부분을 개념적으로 설명해준다.  Life     움직인 시간: 0.5 hr 오늘 야근이 있어서 운동을 못했다.   습관 일기 우리나라 집은 비싼걸까?  화폐를 알고 나니 뭔가 다르게 느껴진다. 공부 좀 빨리할걸 우씨   생체배터리: 40% → 15%  ","categories": ["TIL"],
         "tags": ["TIL"],
-        "url": "/til/2022.07.-28-TIL-&-Diary/",
+        "url": "/til/2022.07.29-TIL-&-Diary/",
         "teaser": "/assets/images/HibikeQauntumSymbol_500x300_version_waifu.jpeg"
       }]
