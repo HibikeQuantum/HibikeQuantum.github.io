d9e610c6ce4a29b5d09eb11e21ab31792b2a41d9
HibikeQuantum <jadetypehoon@gmail.com>
2022-07-29 17:38:27 +0900

    New build and auto commit

---
+++ _posts/2022-07-29-first_post.markdown
@@ -0,0 +1,12 @@
+---
+layout: default
+title:  "about me"
+date:   2022-05-01 12:00:00 +0900
+categories: about
+page: test
+---
+
+![img](assets/img/my-desk.jpeg)
+## I'm back
+Spring has come to my life and I'm back to blogging again. And I bought a good MacBook as a souvenir. Previously, I had a 2013 MacBook. I won't talk much. Cuz the best way to tell me is by posting. Sat, Jun 4, 2022
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "about me",
+        "excerpt":"  I’m back  Spring has come to my life and I’m back to blogging again. And I bought a good MacBook as a souvenir. Previously, I had a 2013 MacBook. I won’t talk much. Cuz the best way to tell me is by posting. Sat, Jun 4, 2022   ","categories": ["about"],
+        "tags": [],
+        "url": "/about/first_post/",
+        "teaser": "/assets/images/HibikeQauntumSymbol_500x300_version_waifu.jpeg"
+      },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated.   Jekyll requires blog post files to be named according to the following format:   YEAR-MONTH-DAY-title.MARKUP   Where YEAR is a four-digit number, MONTH and DAY are both two-digit numbers, and MARKUP is the file extension representing the format used in the file. After that, include the necessary front matter. Take a look at the source for this post to get an idea about how it works.   Jekyll also offers powerful support for code snippets:   def print_hi(name)   puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT.  Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.   ","categories": ["jekyll","update"],
         "tags": [],
